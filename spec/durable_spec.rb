ae7b8318867432d2777aba0fdedb1a2d8cd7a225
Bilal Itani <bilalitani1@gmail.com>
2018-04-20 08:00:35 -0400

    Add tests for ability Durable#add_durability, Durable#remove_durability

--- spec/durable_spec.rb
+++ spec/durable_spec.rb
@@ -2,15 +2,34 @@ require 'rspec'
 require "durable.rb"
 
 RSpec.describe Durable do
+    let(:durable_object) { Class.new { extend Durable } }
+    let (:test_durability) { 100 }
+
+    before { durable_object.set_durability test_durability }
+
     describe "#set_durability" do
-        test_durability = 100
-
-        let(:durable_object) { Class.new { extend Durable } }
-
-        before { durable_object.set_durability test_durability }
-
         it "sets the respective durability" do
             expect(durable_object.durability).to eq test_durability
+        end
+    end
+
+    describe "#add_durability" do
+        let(:additional_durability) {50}
+
+        before {durable_object.add_durability additional_durability}
+
+        it "adds the respective durability" do
+            expect(durable_object.durability).to eq test_durability + additional_durability
+        end
+    end
+
+    describe "#remove_durability" do
+        let(:durability_reduction) {48}
+
+        before {durable_object.remove_durability durability_reduction}
+
+        it "adds the respective durability" do
+            expect(durable_object.durability).to eq test_durability - durability_reduction
         end
     end
 end
