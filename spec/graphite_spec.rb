dafcb4e4d257a02e22ad481cc1e7212d249f8bf1
Bilal Itani <bilalitani1@gmail.com>
2018-04-20 08:24:08 -0400

    Add context blocks to Graphite spec, Reformat Graphite spec

--- spec/graphite_spec.rb
+++ spec/graphite_spec.rb
@@ -6,28 +6,34 @@ RSpec.describe Graphite do
     let(:graphite) { Graphite.new test_durability }
 
     describe "#initialize" do
-        it "sets the respective durability" do
-            expect(graphite.durability).to eq test_durability
+        context 'when creating a new Graphite object' do
+            it "stores the exact durability" do
+                expect(graphite.durability).to eq test_durability
+            end
         end
     end
 
     describe "#add_durability" do
-        let(:additional_durability) {29}
+        let(:additional_durability) { 29 }
 
-        before {graphite.add_durability additional_durability}
+        before { graphite.add_durability additional_durability }
 
-        it "adds the respective durability" do
-            expect(graphite.durability).to eq test_durability + additional_durability
+        context 'when adding durability' do
+            it "stores the respective additional durability" do
+                expect(graphite.durability).to eq test_durability + additional_durability
+            end
         end
     end
 
     describe "#remove_durability" do
-        let(:durability_reduction) {67}
+        let(:durability_reduction) { 67 }
 
-        before {graphite.remove_durability durability_reduction}
+        before { graphite.remove_durability durability_reduction }
 
-        it "removes the respective durability" do
-            expect(graphite.durability).to eq test_durability - durability_reduction
+        context 'when removing durability' do
+            it "subtracts the respective durability reduction" do
+                expect(graphite.durability).to eq test_durability - durability_reduction
+            end
         end
     end
 end
