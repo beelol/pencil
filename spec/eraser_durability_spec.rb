e26f58823d4ac9c4dfa414b07de4d1c81e663e93
Bilal Itani <bilalitani1@gmail.com>
2018-04-20 08:48:29 -0400

    Add eraser test for non-negative durability removal

--- spec/eraser_durability_spec.rb
+++ spec/eraser_durability_spec.rb
@@ -26,13 +26,23 @@ RSpec.describe Eraser do
     end
 
     describe "#remove_durability" do
-        let(:durability_reduction) { 18 }
-
-        before { eraser.remove_durability durability_reduction }
-
         context 'when removing durability' do
+            let(:durability_reduction) { 18 }
+
+            before { eraser.remove_durability durability_reduction }
+            
             it "subtracts the respective durability reduction" do
                 expect(eraser.durability).to eq test_durability - durability_reduction
+            end
+        end
+
+        context 'when reduction value is larger than durability' do
+            let(:durability_reduction) { test_durability + 25 }
+
+            before { eraser.remove_durability durability_reduction }
+
+            it 'does not reach a negative value' do
+                expect(eraser.durability).to eq 0
             end
         end
     end
